52df0624f3713c42fcf97fe41172dbe585b6ebef
guglielmo.padula@aruba.it <gpadula@login.hpc>
2022-01-18 10:18:27 +0100

    first commit

--- tree.R
+++ tree.R
@@ -65,13 +65,14 @@ dev.off()
 png("ompvsmpistrong.png",type='cairo')
 plot(1:24, ompv,lty = 1, lwd = 1, col='red',type='b',pch = 19, xlab="omp (red), mpi(blue)", ylab="time of execution", main="mpi vs omp strong scaling")
 lines(1:24, mpiv,lty = 1, lwd = 1,col='blue',type='b',pch = 19)
-legend(x="topleft",legend=c("omp performance","mpi performance"),col=c("red","blue"),lty=c(1,1),pch=c(19,19))
+legend(x="topright",legend=c("omp performance","mpi performance"),col=c("red","blue"),lty=c(1,1),pch=c(19,19))
 dev.off()
 
 png("strongomp.png",type='cairo')
 plot(1:24, ompv,lty = 1, lwd = 1, col='blue', xlab="number of omp threads", ylab="time of execution", main="Strong omp scaling") 
 mod=lm(ompv~eompv)
-lines(1:24,fitted(mod),col='red');
+lines(1:24,fitted(mod),lty=1,lwd=1,col='red',type="b",pch=19);
+legend(x="topright",legend=c("omp performance","mpi fitted performance"),col=c("blue","red"),lty=c(1,1),pch=c(1,19))
 dev.off()
 
 png("strongmpi.png",type='cairo')
